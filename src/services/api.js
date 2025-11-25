b4dd32fff99861fc2b1079603bf9ce2f3d72f7d3
Niraj Kumar Marandi <157936393+GodinShell@users.noreply.github.com>
2025-11-26 04:45:32 +0530

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "1a7146f28bdab5d0c86c49017db1ce57";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
